e39fa2787a0e78dda45fc4b1b7a48d979aba4fa6
Razvan-Gabriel Geangu <razvan.geangu@gmail.com>
2018-02-02 13:37:17 +0000

    add core data and live connection to ble

---
+++ Navigate/PersistenceService.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceService.swift
+//  Navigate
+//
+//  Created by Răzvan-Gabriel Geangu on 02/02/2018.
+//  Copyright © 2018 Răzvan-Gabriel Geangu. All rights reserved.
+//
+
+import Foundation
