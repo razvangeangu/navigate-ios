9bf6c47257fde2231b2fe615d3fe1650872577c3
Razvan-Gabriel Geangu <razvan.geangu@gmail.com>
2018-02-02 11:15:00 +0000

    first commit

--- Navigate/ViewController.swift
+++ Navigate/ViewController.swift
@@ -2,79 +2,110 @@
 //  ViewController.swift
 //  Navigate
 //
-//  Created by Răzvan-Gabriel Geangu on 05/11/2017.
-//  Copyright © 2017 Răzvan-Gabriel Geangu. All rights reserved.
+//  Created by Răzvan-Gabriel Geangu on 01/02/2018.
+//  Copyright © 2018 Răzvan-Gabriel Geangu. All rights reserved.
 //
 
 import UIKit
-import SceneKit
-import ARKit
+import CoreBluetooth
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+class ViewController: UIViewController {
+    var centralManager: CBCentralManager!
+    var piPeripheral: CBPeripheral!
+    let piServiceCBUUID = CBUUID(string: "0x12AB")
+    let wifiCharacteristicCBUUID = CBUUID(string: "34CD")
+    var data: String = ""
+    var json: Any! {
+        didSet {
+            // print(json)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
-        sceneView.session.run(configuration)
+        // Do any additional setup after loading the view.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        // Dispose of any resources that can be recreated.
     }
+    
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    */
+}
+
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            print("central.state is .poweredOn")
+            centralManager.scanForPeripherals(withServices: [piServiceCBUUID])
+        }
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        piPeripheral = peripheral
+        piPeripheral.delegate = self
+        centralManager.stopScan()
+        centralManager.connect(peripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected")
+        piPeripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print("Disconnected")
+    }
+}
+
+extension ViewController: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
         
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+        
+        for characteristic in characteristics {
+            if characteristic.properties.contains(.notify) {
+                peripheral.setNotifyValue(true, for: characteristic)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        switch characteristic.uuid {
+        case wifiCharacteristicCBUUID:
+            let chunkData = String(bytes: characteristic.value!, encoding: String.Encoding.utf8)!
+            if (chunkData.contains("#done")) {
+                data.append(String(chunkData.dropLast("#done".count)))
+                do {
+                    json = try JSONSerialization.jsonObject(with: data.data(using: .utf8)!)
+                } catch {
+                    print(error)
+                }
+                data = ""
+            } else {
+                data.append(chunkData)
+            }
+        default:
+            print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+        }
     }
 }
